aa463593f0233c2b4f85da7613b950c2d45c6ae3
charlotte1227226 <andy20011019@gmail.com>
2025-07-28 22:51:25 +0800

    first commit

---
+++ class_1/stack.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+char heap_buf[1024];
+int pos = 0;
+
+void *my_malloc(char *stack,int size)
+{
+
+}
+
+int main(){
+    char ch = 65; // char ch = 'A'
+    unsigned char uch = 200;
+}
